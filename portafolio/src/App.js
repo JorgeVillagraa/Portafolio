acd47a7c95ee24c9337a649f8ba021477496b152
JorgeVillagraa <JorgeGamerx12@gmail.com>
2025-04-14 20:40:51 -0300

    feat:Proyecto V1 Finalizado

--- portafolio/src/App.js
+++ portafolio/src/App.js
@@ -1,11 +1,39 @@
 import Home from './Pages/Home';
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import './Styles/Home.css';
 
 function App() {
+  const [showButton, setShowButton] = useState(false);
+
+  // Manejar el evento de scroll
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 300) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  // Función para volver al inicio
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
   return (
- <Home/>
- 
+    <div>
+    <Home />
+    {showButton && (
+      <button className="scroll-to-top" onClick={scrollToTop}>
+        ↑
+      </button>
+    )}
+  </div>
+
   );
 }
 
